0ae1e5722bb70b8bdd3da2375c33d1f20300ba70
ahmdgawad <ahmed-gawad@hotmail.com>
2019-05-25 13:52:04 +0200

    Update AKOAM.py

--- plugin.video.arabicvideos/lib/AKOAM.py
+++ plugin.video.arabicvideos/lib/AKOAM.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from LIBRARY import *
 
-website0a = 'https://akoam.net'
+website0a = 'https://akoam.pro'
 headers = { 'User-Agent' : '' }
 script_name='AKOAM'
 menu_name='[COLOR FFC89008]AKM [/COLOR]'
